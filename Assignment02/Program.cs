1a156f5e8814c6b5f5f4a922cea203ab9262ef89
Chilsi <chilsikc@gmail.com>
2023-02-19 20:38:56 -0500

    Commit

---
+++ Assignment02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Rectangle rect1 = new Rectangle();
+        Console.Clear();
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1. Get Rectangle Length");
+        Console.WriteLine("2. Change Rectangle Length");
+        Console.WriteLine("3. Get Rectangle Width");
+        Console.WriteLine("4. Change Rectangle Width");
+        Console.WriteLine("5. Get Rectangle Perimeter");
+        Console.WriteLine("6. Get Rectangle Area");
+        Console.WriteLine("7. Exit");
+        Console.Write("\r\nSelect an option: ");
+        switch (Console.ReadLine())
+        {
+            case "1":
+                Console.WriteLine("Width = " + rect1.Length.ToString());
+                break;
+            case "2":
+                Console.WriteLine("Enter new length:");
+                Console.ReadLine();
+                break;
+            case "3":
+                Console.WriteLine("Length = " + rect1.Width.ToString());
+                break;
+            case "4":
+                Console.WriteLine("Enter new width:");
+                Console.ReadLine();
+                break;
+            case "5":
+                Console.WriteLine("Perimeter = "+ rect1.Perimeter);
+                break;
+            case "6":
+                Console.WriteLine("Area = "+ rect1.Area);
+                break;
+            case "7":
+                Console.WriteLine("Exit");
+                break;
+            default:
+                Console.WriteLine("Invalid option");
+                break;
+                
+        }
+    }
+}
---
+++ Assignment02/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Rectangle
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Perimeter
+        {
+            get { return (2.0 * Length) + (2.0 * Width); }
+        }
+
+        public double Area
+        {
+            get { return Length * Width; }
+        }
+
+        public Rectangle()
+        {
+            Length = 1.0;
+            Width = 1.0;
+        }
+
+    }
